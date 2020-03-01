c082f7bff87fe23206191776b135528265e921c8
Kritika Sharma <kritika.sharma@metacube.com>
2020-03-01 21:56:34 +0530

    Ead Session 13 Spring Security

---
+++ EadSession13/src/main/java/com/example/demo/EadSession13Application.java
@@ -0,0 +1,17 @@
+/*
+ * @author - Kritika Sharma
+ * @date - 29-02-2020
+ */
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EadSession13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EadSession13Application.class, args);
+	}
+
+}
